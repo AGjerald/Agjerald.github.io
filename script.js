1e0f410f39a34bba1545dacc2c290eb389fb7a7c
Anders Finnebråten Gjerald <114945641+AGjerald@users.noreply.github.com>
2025-09-25 12:21:14 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
-<script>
-  let currentLang = "en"; // default
+let currentLang = "en"; // default
 
-  document.getElementById("lang-btn").addEventListener("click", () => {
-    currentLang = currentLang === "en" ? "no" : "en";
+document.getElementById("lang-btn").addEventListener("click", () => {
+  currentLang = currentLang === "en" ? "no" : "en";
 
-    // Change button label
-    document.getElementById("lang-btn").textContent =
-      currentLang === "en" ? "Norwegian" : "English";
+  // Change button label
+  document.getElementById("lang-btn").textContent =
+    currentLang === "en" ? "Norsk" : "English";
 
-    // Translate all elements with data-en/data-no
-    document.querySelectorAll("[data-en]").forEach(el => {
-      el.textContent = el.getAttribute(`data-${currentLang}`);
-    });
+  // Translate all elements with data-en/data-no
+  document.querySelectorAll("[data-en]").forEach(el => {
+    el.textContent = el.getAttribute(`data-${currentLang}`);
   });
-</script>
+});
